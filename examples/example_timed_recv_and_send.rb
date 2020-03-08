ca7d4eb0070f035a7e0e71ece590e872ef9281cf
0x1eef <0x1eef@protonmail.com>
2020-03-08 03:48:55 +0100

    Update timed example to not include fork

--- examples/example_timed_recv_and_send.rb
+++ examples/example_timed_recv_and_send.rb
@@ -3,19 +3,12 @@
 require 'xchan'
 
 ch = xchan Marshal
-Process.wait fork {
-  # timed_send will return `nil` when it times out.
-  if ! ch.timed_send("Hello parent", 0.5)
-    # handle time out
-  end
-}
-
-# timed_recv will return `nil` when it times out.
-message = ch.timed_recv(0.5)
-if message
+if ! ch.timed_send("Hello parent", 0.5)
+  # handle time out
+end
+if message = ch.timed_recv(0.5)
   puts message
 else
   # handle time out
 end
-
 ch.close
