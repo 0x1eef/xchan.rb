d5592795520621d998905446afbaea56e433f6dd
0x1eef <0x1eef@protonmail.com>
2022-04-25 15:54:02 -0300

    docs: update.

--- readme_examples/1_serializers.rb
+++ readme_examples/1_serializers.rb
@@ -4,27 +4,34 @@ require "xchan"
 ##
 # This channel uses Marshal to serialize objects.
 ch = xchan
-ch.send({msg: "Serialized by Marshal"})
+ch.send(msg: "serialized by Marshal")
 Process.wait fork { print "Received message: ", ch.recv[:msg], "\n" }
 ch.close
 
 ##
 # This channel also uses Marshal to serialize objects.
 ch = xchan(:marshal)
-ch.send({msg: "Serialized by Marshal"})
+ch.send(msg: "serialized by Marshal")
 Process.wait fork { print "Received message: ", ch.recv[:msg], "\n" }
 ch.close
 
 ##
 # This channel uses JSON to serialize objects.
 ch = xchan(:json)
-ch.send({msg: "Serialized by JSON"})
+ch.send(msg: "serialized by JSON")
 Process.wait fork { print "Received message: ", ch.recv["msg"], "\n" }
 ch.close
 
 ##
 # This channel uses YAML to serialize objects.
 ch = xchan(:yaml)
-ch.send({msg: "Serialized by YAML"})
+ch.send(msg: "serialized by YAML")
 Process.wait fork { print "Received message: ", ch.recv[:msg], "\n" }
 ch.close
+
+##
+# == Output
+# Received message: serialized by Marshal
+# Received message: serialized by Marshal
+# Received message: serialized by JSON
+# Received message: serialized by YAML
