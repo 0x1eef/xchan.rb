827437d5a5914a312261e83be62fcd36be529418
0x1eef <0x1eef@protonmail.com>
2022-07-25 13:07:03 -0300

    Lock: update docs.

--- lib/xchan/lock.rb
+++ lib/xchan/lock.rb
@@ -1,26 +1,49 @@
 # frozen_string_literal: true
 
 ##
-# The {Chan::Lock} class is responsible for synchronizing read
-# and write operations on a channel. This class is used to obtain
-# an exclusive lock that only one process may hold at a given time -
-# other processes have to wait before obtaining a lock.
+# {Chan::Lock Chan::Lock} is responsible for synchronizing read and write
+# operations on a channel through an exclusive lock that only one
+# process can hold at a given time.
 class Chan::Lock
   require "lockf"
   require "tempfile"
 
+  ##
+  # @return [Chan::Lock]
+  #  Returns an instance of {Chan::Lock Chan::Lock}.
   def initialize
     @f = Tempfile.new("xchan-lock_file").tap(&:unlink)
   end
 
+  ##
+  # Obtains an exclusive lock. <br>
+  # This method blocks until a lock can be obtained.
+  #
+  # @raise [SystemCallError]
+  #   Might raise a number of Errno exceptions.
+  #
+  # @return [Integer]
+  #  Returns 0 on success.
   def obtain
     @f.lockf(File::F_LOCK, 0)
   end
 
+  ##
+  # Releases the lock.
+  #
+  # @raise [SystemCallError]
+  #   Might raise a number of Errno exceptions.
+  #
+  # @return [Integer]
+  #  Returns 0 on success.
   def release
     @f.lockf(File::F_ULOCK, 0)
   end
 
+  ##
+  #  Closes the underlying IO that's used to implement the lock.
+  #
+  # @return [void]
   def close
     @f.close
   end
